3f8d9c5791bdb3fb415b48f3deb95623968ec19e
Ayc0 <ayc0.benj@gmail.com>
2018-12-10 17:19:15 +0100

    supports viarezo and github

--- login.go
+++ login.go
@@ -8,42 +8,58 @@ import (
 	"strings"
 )
 
+const (
+	viarezo = iota
+	github
+)
+
 var (
-	OAUTH_URL = map[string]string{
-		"viarezo": "https://auth.viarezo.fr/",
-		"github":  "https://github.com/login/",
+	OAUTH = map[string]int{
+		"viarezo": viarezo,
+		"github":  github,
+	}
+
+	OAUTH_URL = map[int]string{
+		viarezo: "https://auth.viarezo.fr/",
+		github:  "https://github.com/login/",
+	}
+
+	OAUTH_AUTHORIZE_URL = map[int]string{
+		viarezo: "https://auth.viarezo.fr/oauth/authorize",
+		github:  "https://github.com/login/oauth/authorize",
+	}
+
+	OAUTH_TOKEN_URL = map[int]string{
+		viarezo: "https://auth.viarezo.fr/oauth/token",
+		github:  "https://github.com/login/oauth/access_token",
 	}
 )
 
-func getOauth() ([]string, map[string]url.Values) {
-	viarezo := "viarezo"
-	github := "github"
-
-	oauths := []string{viarezo, github}
-
-	queries := map[string]url.Values{
+func getOauth() ([]string, map[int]url.Values) {
+	queries := map[int]url.Values{
 		viarezo: url.Values{
 			"scope":         {"default"},
 			"response_type": {"code"},
 		},
 		github: url.Values{},
 	}
-	return oauths, queries
+	return nil, queries
 }
 
 func GetCode(oauth string) (string, error) {
 	_, queries := getOauth()
-	query, ok := queries[oauth]
+	oauthID := OAUTH[oauth]
+	query, ok := queries[oauthID]
 	fmt.Println(query, ok)
 	if !ok {
 		return "", errors.New("oauth doesn't exist")
 	}
 
-	query.Add("redirect_uri", os.Getenv("OAUTH_CALLBACK_URL"))
+	query.Add("redirect_uri", fmt.Sprintf("%s/%s", os.Getenv("OAUTH_CALLBACK_URL"), oauth))
 	query.Add("client_id", os.Getenv(fmt.Sprintf("%s_CLIENT_ID", strings.ToUpper(oauth))))
 	query.Add("state", oauth)
 	return fmt.Sprintf(
-		"%soauth/authorize/?%s",
-		os.Getenv(OAUTH_URL[oauth]),
+		"%s?%s",
+		OAUTH_AUTHORIZE_URL[oauthID],
 		query.Encode()), nil
 }
--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"net/http"
 
@@ -27,45 +28,69 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.GET("/callback", func(c echo.Context) error {
+	e.GET("/callback/viarezo", func(c echo.Context) error {
+		oauth := c.QueryParam("state")
 		body := req.Param{
 			"grant_type":    "authorization_code",
 			"code":          c.QueryParam("code"),
-			"redirect_uri":  os.Getenv("OAUTH_CALLBACK_URL"),
-			"client_id":     os.Getenv("VIAREZO_CLIENT_ID"),
-			"client_secret": os.Getenv("VIAREZO_CLIENT_SECRET"),
+			"redirect_uri":  fmt.Sprintf("%s/%s", os.Getenv("OAUTH_CALLBACK_URL"), oauth),
+			"client_id":     os.Getenv(fmt.Sprintf("%s_CLIENT_ID", strings.ToUpper(oauth))),
+			"client_secret": os.Getenv(fmt.Sprintf("%s_CLIENT_SECRET", strings.ToUpper(oauth))),
 		}
+
 		res, err := req.Post(
-			fmt.Sprintf("%soauth/token", OAUTH_URL[c.QueryParam("state")]),
+			OAUTH_TOKEN_URL[OAUTH[oauth]],
 			body,
 		)
 
 		if err != nil {
+			return err
 		}
-
-		fmt.Println(res)
 
 		token := Token{} // empty token
 		err = res.ToJSON(&token)
 		if err != nil {
-			panic(err)
+			return err
 		}
 
 		return c.JSONPretty(http.StatusOK, token, "    ")
 	})
-	e.GET("/login_viarezo", func(c echo.Context) error {
+	e.GET("/callback/github", func(c echo.Context) error {
+		oauth := c.QueryParam("state")
+		body := req.Param{
+			"code":          c.QueryParam("code"),
+			"redirect_uri":  fmt.Sprintf("%s/%s", os.Getenv("OAUTH_CALLBACK_URL"), oauth),
+			"client_id":     os.Getenv(fmt.Sprintf("%s_CLIENT_ID", strings.ToUpper(oauth))),
+			"client_secret": os.Getenv(fmt.Sprintf("%s_CLIENT_SECRET", strings.ToUpper(oauth))),
+			"state":         oauth,
+		}
+
+		res, err := req.Post(
+			OAUTH_TOKEN_URL[OAUTH[oauth]],
+			req.BodyJSON(&body),
+		)
+
+		if err != nil {
+			return err
+		}
+
+		token, err := res.ToString()
+
+		return c.String(http.StatusOK, token)
+	})
+	e.GET("/login/viarezo", func(c echo.Context) error {
 		url, err := GetCode("viarezo")
 		if err != nil {
 		}
 		fmt.Println(url)
-		return c.Redirect(http.StatusMovedPermanently, url)
+		return c.Redirect(http.StatusTemporaryRedirect, url)
 	})
-	e.GET("/login_github", func(c echo.Context) error {
+	e.GET("/login/github", func(c echo.Context) error {
 		url, err := GetCode("github")
 		if err != nil {
 		}
 		fmt.Println(url)
-		return c.Redirect(http.StatusMovedPermanently, url)
+		return c.Redirect(http.StatusTemporaryRedirect, url)
 	})
 	e.Logger.Fatal(e.Start(":80"))
 }
